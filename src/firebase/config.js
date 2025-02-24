94e20f998728d98f644e7a84ba50dfc7c4a272d8
Jemima Weld <136545759+jweld-codes@users.noreply.github.com>
2025-02-24 03:34:06 -0600

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore, collection, doc, query, where } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAy49QW6GQx7EIvFy_q8ghQEkYMXKIRLiI",
-  authDomain: "unirec-project.firebaseapp.com",
-  projectId: "unirec-project",
-  storageBucket: "unirec-project.appspot.com",
-  messagingSenderId: "212185119630",
-  appId: "1:212185119630:web:12b0850902387521ac82df"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
